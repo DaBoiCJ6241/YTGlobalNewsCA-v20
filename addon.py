46b37975a2572c50f06ab509cc77f01f5d8ff396
leopheard <leopheard.misc@gmail.com>
2019-08-29 17:02:57 -0400

    upload

--- addon.py
+++ addon.py
@@ -2,17 +2,17 @@ from xbmcswift2 import Plugin, xbmcgui
 from resources.lib import mainaddon
 
 plugin = Plugin()
-URL0 = "https://live.corusdigitaldev.com/groupb/live/6512f46a-8730-4668-af48-83159f3c706d/live.isml/live-audio_1=96000-video=2499968-261185081.ts"
-URL1 = "https://www.youtube.com/feeds/videos.xml?channel_id=UChLtXXpo4Ge1ReTEboVvTDg"#GLOBALNEWS
-URL1 = "https://www.youtube.com/feeds/videos.xml?channel_id=UCkiP0kvvwqUWlttRLCq88gw"#GLOBALTV
-URL3 = "https://www.youtube.com/feeds/videos.xml?channel_id=UCxUD8G1jO8T-Ef2tuADCZOA"#16x9
+url0 = "https://live.corusdigitaldev.com/groupb/live/6512f46a-8730-4668-af48-83159f3c706d/live.isml/live-audio_1=96000-video=2499968-261185081.ts"
+url1 = "https://www.youtube.com/feeds/videos.xml?channel_id=UChLtXXpo4Ge1ReTEboVvTDg"#GLOBALNEWS
+url1 = "https://www.youtube.com/feeds/videos.xml?channel_id=UCkiP0kvvwqUWlttRLCq88gw"#GLOBALTV
+url3 = "https://www.youtube.com/feeds/videos.xml?channel_id=UCxUD8G1jO8T-Ef2tuADCZOA"#16x9
 
 @plugin.route('/')
 def main_menu():
     items = [
         {
             'label': plugin.get_string(30000), 
-            'path': "https://live.corusdigitaldev.com/groupb/live/6512f46a-8730-4668-af48-83159f3c706d/live.isml/live-audio_1=96000-video=2499968-261185081.ts",
+            'path': "https://globalnewsdigitalvideo.corusdigitaldev.com/news/nhsbwgj9tj-567t8d5s47/Scheersamesexmarriage_video.mp4",
             'thumbnail': "https://i0.wp.com/media.globalnews.ca/videostatic/349/131/Global%20News__946999.png?w=500&quality=70&strip=all", 
             'is_playable': True},
    {
@@ -33,21 +33,21 @@ def main_menu():
 
 @plugin.route('/GN_episodes/')
 def GN_episodes():
-    soup1 = mainaddon.get_soup1(URL1)
+    soup1 = mainaddon.get_soup1(url1)
     playable_podcast1 = mainaddon.get_playable_podcast(soup1)
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
     return items
 
 @plugin.route('/GTV_episodes/')
 def GTV_episodes():
-    soup2 = mainaddon.get_soup2(URL2)
+    soup2 = mainaddon.get_soup2(url2)
     playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
     items = mainaddon.compile_playable_podcast2(playable_podcast2)
     return items
 
 @plugin.route('/SBN_episodes/')
 def SBN_episodes():
-    soup3 = mainaddon.get_soup3(URL3)
+    soup3 = mainaddon.get_soup3(url3)
     playable_podcast3 = mainaddon.get_playable_podcast3(soup3)
     items = mainaddon.compile_playable_podcast3(playable_podcast3)
     return items
